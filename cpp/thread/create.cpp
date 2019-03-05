2234cc254ab4656ae1c31cd1fa49e9db8136f1c3
Fan Wu <deepexploring@gmail.com>
2019-03-05 14:16:16 -0800

    Update create.cpp

--- cpp/thread/create.cpp
+++ cpp/thread/create.cpp
@@ -1,29 +1,45 @@
+#include <chrono>
 #include <iostream>
 #include <string>
 #include <thread>
-#include <chrono>
+#include <vector>
 
 using namespace std;
 
-
-void thread_fn(int n)
+struct ThreadParameter
 {
-    if (n == 2)
+    ThreadParameter(vector<int> *shared, int *index, const string& thread_name) : m_shared(shared), m_size(shared->size()), m_current(index), m_thread_name(thread_name){}
+
+    vector<int> *m_shared;
+    int m_size;
+    int *m_current;
+    string m_thread_name;
+};
+
+void thread_fn(void *param)
+{
+    ThreadParameter *l_param = (ThreadParameter *) param;
+    while (*(l_param->m_current) < l_param->m_size)
     {
-        thread t4(thread_fn, 4);
-        t4.join();
+    ¦   printf("This is the thread %s, show the value at index %d. \n", l_param->m_thread_name.c_str(), ++(*(l_param->m_current)));
     }
-    this_thread::sleep_for(chrono::seconds(n));
-    cout << "The thread " << n << " is executing." << this_thread::get_id() << endl;
 }
 
 int main()
 {
     cout << "This is the program to show how to create a thread in C++ 11." << endl;
 
-    thread t1(thread_fn, 1);
-    thread t2(thread_fn, 2);
-    thread t3(thread_fn, 3);
+    vector<int> shared(100, 1);
+    int index = 0;
+
+    ThreadParameter param1(&shared, &index, "first");
+    thread t1(thread_fn, &param1);
+
+    ThreadParameter param2(&shared, &index, "second");
+    thread t2(thread_fn, &param2);
+
+    ThreadParameter param3(&shared, &index, "third");
+    thread t3(thread_fn, &param3);
 
     t1.join();
     t2.join();
