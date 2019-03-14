faf23227f96539fd373af9fd091fb1f5bac226d2
Fan Wu <deepexploring@gmail.com>
2019-03-14 14:47:33 -0700

    Update create_new.cpp

--- cpp/thread/create_new.cpp
+++ cpp/thread/create_new.cpp
@@ -6,41 +6,44 @@
 
 extern "C"
 {
-  #include <stdio.h>
+    #include <stdio.h>
 }
 
-#define size 30
+#define g_size 30
 #define num_of_thread 3
 
 using namespace std;
 
 struct ConcurrentList
 {
-  explicit ConcurrentList(vector<int> &in_list, int &current) :
-    m_list(in_list),
-    m_current(current) {}
+    explicit ConcurrentList(int length, int current) : 
+        m_list(vector<int>(length, 1)), 
+        m_current(current) {}
 
-  vector<int> &m_list;
-  int &m_current;
+    vector<int> m_list;
+    int m_current;
 };
 
 void thread_func(ConcurrentList &l, const string &thread_id)
 {
-    if (l.m_current < l.m_list.size())
+    while (l.m_current < l.m_list.size())
         printf("The thread %s moved the index %d.\n", thread_id.c_str(), l.m_current++);
 }
 
 int main()
 {
-  cout << "This is the program to show how to create a thread in C++ 11." << endl;
+    cout << "This is the program to show how to create a thread in C++ 11." << endl;
 
-  vector<int> shared(100, 1);
-  for(int i = 0; i < size; ++i)
-    shared[i] = i;
+    
+    int shared_index = 0;
 
-  int shared_index = 0;
+    ConcurrentList cl(g_size, shared_index);
 
-  ConcurrentList cl(std::ref(shared), std::ref(shared_index));
+    thread t1(thread_func, std::ref(cl), "writer");
+    thread t2(thread_func, std::ref(cl), "reader");
 
-  return 1;
+    t1.join();
+    t2.join();
+
+    return 1;
 }
