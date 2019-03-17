97b273177bb06f40f211130b9c51bc033ab5bc3a
Fan Wu <deepexploring@gmail.com>
2019-03-16 23:58:15 -0700

    Update conditional_variable.cpp

--- cpp/thread/conditional_variable.cpp
+++ cpp/thread/conditional_variable.cpp
@@ -49,3 +49,50 @@ void thread_add(ConcurrentList &cl, const string &thread_name)
     ¦
     ¦   cl.m_update = true;
     ¦   cl.m_process = false;
+
+    ¦   cl.m_cv.notify_one();
+    }
+}
+
+void thread_show_and_update(ConcurrentList &cl, const string &thread_name)
+{
+    while(cl.m_count < number_seq)
+    {
+    ¦   unique_lock<mutex> update_lock(cl.m_mutex);
+
+    ¦   while(!cl.m_update)
+    ¦   ¦   cl.m_cv.wait(update_lock);
+    ¦
+    ¦   printf(", %d", cl.m_shared_memory[2]);
+
+    ¦   cl.m_shared_memory[0] = cl.m_shared_memory[1];
+    ¦   cl.m_shared_memory[1] = cl.m_shared_memory[2];
+
+    ¦   cl.m_update = false;
+    ¦   cl.m_process = true;
+
+    ¦  cl.m_cv.notify_one();
+    }
+}
+
+int main()
+{
+    cout << "This program shows how to use mutex to sync." << endl;
+
+    ConcurrentList cl(3);
+
+    cl.m_shared_memory[0] = 1;
+    cl.m_shared_memory[1] = 1;
+
+    printf("The fibonacci seq is %d, %d", 1, 1);
+
+    thread update(thread_show_and_update, std::ref(cl), "show and update thread");
+    thread addition(thread_add, std::ref(cl), "adding thread");
+
+    addition.join();
+    update.join();
+
+    printf(".\n");
+
+    return 1;
+}
