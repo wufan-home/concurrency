f059c8c19cee50926ca24d5d6a18996f477988f9
Fan Wu <deepexploring@gmail.com>
2019-03-18 22:33:10 -0700

    Create cv.cpp

---
+++ cpp/thread/cv.cpp
@@ -0,0 +1,87 @@
+#include <chrono>
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
+extern "C"
+{
+    #include <stdio.h>
+}
+
+using namespace std;
+
+#define memeory_size 3
+#define number_seq 10
+
+
+struct ConcurrentList
+{
+    explicit ConcurrentList(int length) :
+    ¦  m_shared_memory(vector<int>(length, 0)),
+    ¦  m_update(false) {}
+
+    vector<int> m_shared_memory;
+
+    mutex m_mutex;
+    condition_variable m_cv;
+    bool m_update;
+};
+
+void thread_add(ConcurrentList &cl, const string &thread_name)
+{
+    for(int i = 0; i < 10; ++i)
+    {
+    ¦   this_thread::sleep_for(chrono::seconds(1));
+    ¦   unique_lock<mutex> process_lock(cl.m_mutex, defer_lock);
+    ¦   if(process_lock.try_lock())
+    ¦   {
+    ¦   ¦   cl.m_shared_memory[2] = cl.m_shared_memory[0] + cl.m_shared_memory[1];
+    ¦   ¦   cl.m_update = true;
+    ¦   ¦   cl.m_cv.notify_one();
+    ¦   }
+    }
+}
+
+void thread_show_and_update(ConcurrentList &cl, const string &thread_name)
+{
+    while(true)
+    {
+    ¦  unique_lock<mutex> update_lock(cl.m_mutex);
+        while(!cl.m_update)
+    ¦   ¦ cl.m_cv.wait(update_lock);
+
+    ¦  printf("update: %d\n", cl.m_shared_memory[2]);
+
+    ¦  cl.m_shared_memory[0] = cl.m_shared_memory[1];
+    ¦  cl.m_shared_memory[1] = cl.m_shared_memory[2];
+
+    ¦  cl.m_update = false;
+
+    ¦ cl.m_cv.notify_one();
+    }
+}
+
+int main()
+{
+    cout << "This program shows how to use mutex to sync." << endl;
+
+    ConcurrentList cl(3);
+
+    cl.m_shared_memory[0] = 1;
+    cl.m_shared_memory[1] = 1;
+
+    printf("The fibonacci seq is %d, %d", 1, 1);
+
+    thread update(thread_show_and_update, std::ref(cl), "show and update thread");
+    thread addition(thread_add, std::ref(cl), "adding thread");
+
+    addition.join();
+    update.join();
+
+    printf(".\n");
+
+    return 1;
+}
