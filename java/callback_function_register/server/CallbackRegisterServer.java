ae7f30585cbd1bdc7421d169cd9b9e035b6fa160
Fan Wu <fanwu@Fans-MacBook-Air.local>
2019-07-01 08:51:00 -0700

    Add the smaple code of multithreading.

--- java/callback_function_register/server/CallbackRegisterServer.java
+++ java/callback_function_register/server/CallbackRegisterServer.java
@@ -1,9 +1,33 @@
 public class CallbackRegisterServer
 {
+    public void register_function()
+    {
+
+    }
+    
+    public void register_callback()
+    {
+        // This function is to register objects which
+        // provide callback functions.
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Start the program CallbackRegisterServer");
+        // System.out.println("Start the program CallbackRegisterServer");
+        
+        Runnable task = () -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Hello " + threadName);
+        };
 
-        System.out.println("Stop the program CallbackRegisterServer");
+        task.run();
+
+        Thread thread = new Thread(task);
+        thread.start();
+
+        System.out.println("Done!");
+
+
+        // System.out.println("Stop the program CallbackRegisterServer");
     }
 }
