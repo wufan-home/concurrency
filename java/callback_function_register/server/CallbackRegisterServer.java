7c1f276b97c4f846f3b5d06bb3dc5bc020fa6407
Fan Wu <fanwu@Fans-MacBook-Air.local>
2019-07-02 23:41:22 -0700

    First runnable version.

--- java/callback_function_register/server/CallbackRegisterServer.java
+++ java/callback_function_register/server/CallbackRegisterServer.java
@@ -1,32 +1,117 @@
+import java.lang.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class CallbackRegisterServer
 {
-    public void register_function()
+    private final static class Request
     {
+        private String mContext;
 
+        Request(final String context)
+        {
+            mContext = context;
+        }
+
+        public String getContext()
+        {
+            return mContext;
+        }
+
+        public void callback()
+        {
+            System.out.println(mContext + " callback function is run.");
+        }
+    } 
+    
+    private static boolean eventIsTriggered = false;
+
+    private static ConcurrentLinkedQueue<Request> concurrentLinkedQueue = new ConcurrentLinkedQueue<Request>();
+
+    private static int size = 30; 
+    
+    public static void registerCallback(final Request request)
+    {
+        concurrentLinkedQueue.offer(request);
+        System.out.println(request.getContext() + " is registered. " + Integer.toString(concurrentLinkedQueue.size()));
     }
     
-    public void register_callback()
+    public static void executeCallback(final Request request)
     {
-        // This function is to register objects which
-        // provide callback functions.
+        request.callback();
     }
 
     public static void main(String[] args)
     {
         // System.out.println("Start the program CallbackRegisterServer");
-        
-        Runnable task = () -> {
-            String threadName = Thread.currentThread().getName();
-            System.out.println("Hello " + threadName);
+
+        Runnable sendRequest = () -> {
+            try 
+            {
+                for (int i = 0; i < size; ++i)
+                {
+                    Request request = new Request("test request " + Integer.toString(i));
+                    Thread.sleep(1000);
+                    registerCallback(request);
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error: Fail to sleep for 1000.");
+            }
         };
 
-        task.run();
+        Thread threadRequest = new Thread(sendRequest);
+        threadRequest.start();
 
-        Thread thread = new Thread(task);
-        thread.start();
+        Runnable eventTrigger = () -> {
+            try
+            {
+                Thread.sleep(4500);
+                eventIsTriggered = true;
+                System.out.println("The event is triggered.");
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error Failed to trigger the event.");
+            }
+        };
 
-        System.out.println("Done!");
+        Thread threadEventTriggered = new Thread(eventTrigger);
+        threadEventTriggered.start();
 
+        Runnable executeCallbackRunnable = () -> {
+            for(int i = 0; i < size; )
+            {
+                if(eventIsTriggered)
+                {
+                    if (concurrentLinkedQueue.size() > 0)
+                    {
+                        executeCallback(concurrentLinkedQueue.poll());
+                        ++i;
+                    }
+                    else
+                    {
+                        System.out.println("The request queue is empty. Wait for more requests be pushed.");
+                    }
+                }
+                else
+                {
+                    System.out.println("The event has not been triggered.");
+                }
+
+                try
+                {
+                    Thread.sleep(500);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        };
+
+        Thread threadExecuteCallback = new Thread(executeCallbackRunnable);
+        threadExecuteCallback.start();
 
         // System.out.println("Stop the program CallbackRegisterServer");
     }
